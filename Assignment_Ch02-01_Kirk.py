36b98694a1138699a6905590a76e5ba4cb86e72e
Jamey Kirk <JameyKirk@gmail.com>
2022-01-14 23:03:54 +0900

    Initial commit

---
+++ Assignment_Ch02-01_Kirk.py
@@ -0,0 +1,12 @@
+num1 = 125
+num2 = 28
+num3 = -25
+average = (num1 + num2 + num3) / 3
+
+print("num1 is ", num1)
+print("\n")
+print("num2 is ", num2)
+print("\n")
+print("num3 is ", num3)
+print("\n")
+print("The average of num1, num2, and num3 is {:5.2f}".format(average))
